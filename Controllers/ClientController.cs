d81f35799cf8a363da3396d250643beb35b176aa
Otavio Augusto <134602837+0tavioPascoal@users.noreply.github.com>
2025-07-24 03:23:21 -0300

    feat: Starting the development of the client screen

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers {
+    public class ClientController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult CreateClient() {
+            return View();
+        }
+
+        public IActionResult EditClient() {
+            return View();
+        }
+
+        public IActionResult DeleteClient() {
+            return View();
+        }
+    }
+}
